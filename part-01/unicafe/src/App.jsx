a3e4afdf421da8af9873158d93dae45ec3153c90
Diana Stanciu <diana.frontdev@gmail.com>
2025-01-27 08:11:22 +0200

    part-01

--- part-01/unicafe/src/App.jsx
+++ part-01/unicafe/src/App.jsx
@@ -95,10 +95,41 @@
 //endregion
 
 //region part-01 c: page re-rendering
-const App = (props) => {
-    const {counter} = props
-    return (
-        <div>{counter}</div>
+import { useState } from 'react';
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
+
+const App = () => {
+    const [counter, setCounter] = useState(0);
+
+    console.log('rendering with counter value', counter);
+
+    const increaseByOne = () => {
+        console.log('increasing, value before', counter)
+        setCounter(counter + 1)
+    }
+    const decreaseByOne = () => {
+        console.log('decreasing value before', counter)
+        setCounter(counter - 1)
+    }
+    const setToZero = () => {
+        console.log('resetting to zero, value before', counter)
+        setCounter(0)
+    }
+
+    return(
+        <>
+            <Display counter={counter} />
+            <Button onClick={increaseByOne} text="plus"></Button>
+            <Button onClick={setToZero} text="reset"></Button>
+            <Button onClick={decreaseByOne} text="minus"></Button>
+        </>
     )
 }
 //endregion
--- part-01/unicafe/src/main.jsx
+++ part-01/unicafe/src/main.jsx
@@ -2,12 +2,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-
-//region part-01 c: page re-rendering
-let counter = 1
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
-)
-//endregion
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)

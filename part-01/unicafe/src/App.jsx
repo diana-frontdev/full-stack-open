c2b4809107c64750b7572cce00ffed1f7752c541
Diana Stanciu <diana.frontdev@gmail.com>
2025-01-28 09:47:57 +0200

    part-01

--- part-01/unicafe/src/App.jsx
+++ part-01/unicafe/src/App.jsx
@@ -95,41 +95,72 @@
 //endregion
 
 //region part-01 c: page re-rendering
-import { useState } from 'react';
+// import { useState } from 'react';
+//
+// const Display = ({counter}) => <div>{counter}</div>
+//
+// const Button = ({onClick, text}) => (
+//     <button onClick={onClick}>
+//         {text}
+//     </button>
+// )
+//
+// const App = () => {
+//     const [counter, setCounter] = useState(0);
+//
+//     console.log('rendering with counter value', counter);
+//
+//     const increaseByOne = () => {
+//         console.log('increasing, value before', counter)
+//         setCounter(counter + 1)
+//     }
+//     const decreaseByOne = () => {
+//         console.log('decreasing value before', counter)
+//         setCounter(counter - 1)
+//     }
+//     const setToZero = () => {
+//         console.log('resetting to zero, value before', counter)
+//         setCounter(0)
+//     }
+//
+//     return(
+//         <>
+//             <Display counter={counter} />
+//             <Button onClick={increaseByOne} text="plus"></Button>
+//             <Button onClick={setToZero} text="reset"></Button>
+//             <Button onClick={decreaseByOne} text="minus"></Button>
+//         </>
+//     )
+// }
+//endregion
 
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = ({onClick, text}) => (
-    <button onClick={onClick}>
-        {text}
-    </button>
-)
+//region part-01 d: a more complex debugging react apps
+import {useState} from 'react';
 
 const App = () => {
-    const [counter, setCounter] = useState(0);
+    const [clicks, setClicks] = useState({
+        left: 0, right: 0
+    })
 
-    console.log('rendering with counter value', counter);
-
-    const increaseByOne = () => {
-        console.log('increasing, value before', counter)
-        setCounter(counter + 1)
-    }
-    const decreaseByOne = () => {
-        console.log('decreasing value before', counter)
-        setCounter(counter - 1)
-    }
-    const setToZero = () => {
-        console.log('resetting to zero, value before', counter)
-        setCounter(0)
+    const handleLeftClick = () => {
+        setClicks({...clicks, left: clicks.left + 1})
     }
 
-    return(
-        <>
-            <Display counter={counter} />
-            <Button onClick={increaseByOne} text="plus"></Button>
-            <Button onClick={setToZero} text="reset"></Button>
-            <Button onClick={decreaseByOne} text="minus"></Button>
-        </>
+    const handleRightClick = () => {
+        setClicks({...clicks, right: clicks.right + 1})
+    }
+
+    return (
+        <div>
+            {clicks.left}
+            <button onClick={handleLeftClick}>
+                left
+            </button>
+            <button onClick={handleRightClick}>
+                right
+            </button>
+            {clicks.right}
+        </div>
     )
 }
 //endregion
